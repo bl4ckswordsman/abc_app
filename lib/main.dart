032c6d81dc539cc77df7549b3a3002e0ba984bbc
amarildo <amarildo.rajta@gmail.com>
2023-08-15 14:53:38 +0200

    letter buttons have the same size now

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -25,11 +24,13 @@ class ABCapp extends StatelessWidget {
 
 
 class MyAppState extends ChangeNotifier {
-  var current = generateWordPairs().first;
+  bool isSwedish = false;
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<String> alphabet = List.generate(26, (index) => String.fromCharCode('A'.codeUnitAt(0) + index));
+  final List<String> englishAlphabet = List.generate(26, (index) => String.fromCharCode('A'.codeUnitAt(0) + index));
+  List<String> get swedishAlphabet =>
+      List.from(englishAlphabet)..addAll(['Å', 'Ä', 'Ö']); // Add the Swedish letters
 
   @override
   Widget build(BuildContext context) {
@@ -45,7 +46,7 @@ class MyHomePage extends StatelessWidget {
               spacing: 16.0,
               runSpacing: 16.0,
               children: [
-                for (var letter in alphabet)
+                for (var letter in englishAlphabet)
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: AlphabetButton(letter: letter),
@@ -68,22 +69,21 @@ class AlphabetButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        // Handle button press
-      },
-      child: Text(letter),
-      style: ElevatedButton.styleFrom(
-        shape: CircleBorder(),  // Make the button circular
-        primary: Colors.blue,
-        onPrimary: Colors.white,
-        padding: EdgeInsets.all(40.0),  // Increase padding for a bigger button
-        textStyle: TextStyle(fontSize: 50.0),
+    return SizedBox(
+      width: 120.0, // Set a fixed width for all buttons
+      height: 120.0, // Set a fixed height for all buttons
+      child: ElevatedButton(
+        onPressed: () {
+          // Handle button press
+        },
+        child: Text(letter),
+        style: ElevatedButton.styleFrom(
+          shape: CircleBorder(),  // Make the button circular
+          primary: Colors.blue,
+          onPrimary: Colors.white,
+          textStyle: TextStyle(fontSize: 60.0),
+        ),
       ),
     );
   }
 }
-
-
-
-
