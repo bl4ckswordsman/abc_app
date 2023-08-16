dee24ab3f02d4f32a8ae0d7f6758f186abc2ad62
amarildo <amarildo.rajta@gmail.com>
2023-08-16 17:17:46 +0200

    added non funct. lang. selector. fixed appbar width and removed spacing of scroll view

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,59 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
 
 void main() {
   runApp(const ABCapp());
 }
+
+enum Language {
+  swedish,
+  english,
+}
+
+class LanguageDropdown extends StatefulWidget {
+  const LanguageDropdown({super.key});
+
+  @override
+  State<LanguageDropdown> createState() => _LanguageDropdownState();
+}
+
+class _LanguageDropdownState extends State<LanguageDropdown> {
+  Language _language = Language.swedish;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownMenu<Language>(
+      initialSelection: Language.swedish,
+      label: const Text('Language'),
+      onSelected: (Language? newValue) {
+        setState(() {
+          _language = newValue!;
+        });
+      },
+      dropdownMenuEntries: <DropdownMenuEntry<Language>>[
+        DropdownMenuEntry<Language>(
+          value: Language.swedish,
+          label: 'Svenska',
+        ),
+        DropdownMenuEntry<Language>(
+          value: Language.english,
+          label: 'English',
+        ),
+      ],
+    );
+  }
+}
+
+  String getLanguageLabel(Language language) {
+    switch (language) {
+      case Language.swedish:
+        return 'Svenska';
+      case Language.english:
+        return 'English';
+      default:
+        return '';
+    }
+  }
+
 
 class ABCapp extends StatelessWidget {
   const ABCapp({super.key});
@@ -22,26 +71,27 @@ class ABCapp extends StatelessWidget {
   }
 }
 
-
-class MyAppState extends ChangeNotifier {
-  bool isSwedish = false;
-}
-
 class MyHomePage extends StatelessWidget {
   final List<String> englishAlphabet = List.generate(26, (index) => String.fromCharCode('A'.codeUnitAt(0) + index));
-  List<String> get swedishAlphabet =>
-      List.from(englishAlphabet)..addAll(['Å', 'Ä', 'Ö']); // Add the Swedish letters
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        toolbarHeight: 100,
         title: const Text('ABC app'),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40.0),
+            child: LanguageDropdown(),
+          ), // Add the LanguageDropdown widget to the app bar
+        ],
       ),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: SingleChildScrollView(
+            clipBehavior: Clip.none,
             child: Wrap(
               spacing: 16.0,
               runSpacing: 16.0,
@@ -60,8 +110,6 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
-
-
 class AlphabetButton extends StatelessWidget {
   final String letter;
 
@@ -78,9 +126,7 @@ class AlphabetButton extends StatelessWidget {
         },
         child: Text(letter),
         style: ElevatedButton.styleFrom(
-          shape: CircleBorder(),  // Make the button circular
-          primary: Colors.blue,
-          onPrimary: Colors.white,
+          foregroundColor: Colors.white, backgroundColor: Colors.blue, shape: CircleBorder(),
           textStyle: TextStyle(fontSize: 60.0),
         ),
       ),
