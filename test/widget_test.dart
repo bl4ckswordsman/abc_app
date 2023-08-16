6f846f872620d5c32c95846331637bcf8a4b9a22
amarildo <amarildo.rajta@gmail.com>
2023-08-16 18:56:32 +0200

    fixed typo

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,10 +10,11 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:abc_app/main.dart';
 
+
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const ABCapp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
